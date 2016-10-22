5a002db690878e1e88c539e8a5ae2da98cf6c263
Jean-Luc Desroches <Jean-Luc Desroches>
2016-10-22 10:08:13 -0400

    running scaffold

---
+++ src/app/index.module.js
@@ -0,0 +1,6 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('olcg', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+})();
